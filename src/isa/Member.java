82cbbfcf3c168e328f0b3979e1bed448ee7fe8ee
Lc24aam <LC24AAM@herts.ac.uk>
2026-04-07 16:50:39 +0100

    Lacie- Added in member class attributes and 2 functions.

--- src/isa/Member.java
+++ src/isa/Member.java
@@ -3,7 +3,23 @@ This Page was coded by Lacie Carey.
  */
 package isa;
 
+import java.util.ArrayList;
+
 
 public class Member {
     
+    private String name;
+    private String postalAddress;
+    private String email;
+    private int itemsDonated;
+    private ArrayList<Item> borrowedItems;
+    
+}
+
+public int getMaxBorrowableItems(){
+    return Math.min(5, itemsDonated);
+}
+
+public boolean canBorrowMore(){
+    return borrowedItems.size() < getMaxBorrowableItems();
 }
