f7a600f18053f143b39c990a6e901163818c3988
Lc24aam <LC24AAM@herts.ac.uk>
2026-04-12 14:46:03 +0100

    Lacie- Getters and setters are added, need improvement.

--- src/isa/Member.java
+++ src/isa/Member.java
@@ -4,6 +4,7 @@ This Page was coded by Lacie Carey.
 package isa;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Member {
@@ -14,13 +15,70 @@ public class Member {
     private int itemsDonated;
     private ArrayList<Item> borrowedItems;
     
+    public Member(String name, String postalAddress, String email, int itemsDonated) {
+        this.name = name;
+        this.postalAddress = postalAddress;
+        this.email = email;
+        this.itemsDonated = itemsDonated;
+        this.borrowedItems = new ArrayList<>();
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getPostalAddress(){
+        return postalAddress;
+    }
+    
+    public void setPostalAddress(String postalAddress) {
+        this.postalAddress = postalAddress;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int getItmesDonated() {
+        return itemsDonated;
+    }
+    
+    public ArrayList<Item> getBorrowedItems() {
+        return Collections.unmodifiableArrayList(borrowedItems);
+    }
+    
+    
+    
+    
     public int getMaxBorrowableItems(){
-    return Math.min(5, itemsDonated);
+        return Math.min(5, itemsDonated);
     }
     
     public boolean canBorrowMore(){
-    return borrowedItems.size() < getMaxBorrowableItems();
+        return borrowedItems.size() < getMaxBorrowableItems();
     }
     
+    public void donateItem() {
+        itemsDonated++;
+    }
     
+    public boolean borrowItem(Item item){
+        if(!canBorrowMore()){
+            return false;
+        }
+        borrowedItems.add(item);
+        return true;       
+    }
+    
+    public boolean returnItem(Item item) {
+        return borrowedItems.remove(item);
+    }
 }
