00cdf046f45458f015d024bd442947821232241b
Lc24aam <LC24AAM@herts.ac.uk>
2026-04-12 15:25:16 +0100

    Lacie- Starting DVD

--- src/isa/DVD.java
+++ src/isa/DVD.java
@@ -3,7 +3,34 @@ This Page was coded by Lacie Carey.
  */
 package isa;
 
+import java.util.List;
 
-public class DVD {
+
+public abstract class Item {
+    private String title;
+    private String language;
+    private Member donor;
+    private Member borrower;
+    
+    public Item (String title, String language, Member donor){
+        this.title = title;
+        this.language = language;
+        this.donor = donor;
+        this.borrower = null;
+    }
+    
+    public boolean isOnLoan(){
+        return borrower != null;
+    }
+    
+    public boolean lendTo(Member member){
+        if (isOnLoan()) return false;
+        borrower = member;
+        return true;
+    }
+    
+    public void returnItem(){
+        borrower = null;
+    }
     
 }
