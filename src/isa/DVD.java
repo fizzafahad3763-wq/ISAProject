e1460fd4672d63fb3742f355fed86e4cd1406219
Lc24aam <LC24AAM@herts.ac.uk>
2026-04-14 17:00:37 +0100

    Lacie- finishing DVD.

--- src/isa/DVD.java
+++ src/isa/DVD.java
@@ -3,34 +3,42 @@ This Page was coded by Lacie Carey.
  */
 package isa;
 
+import java.util.Collections;
 import java.util.List;
 
 
-public abstract class DVD {
-    private String title;
-    private String language;
-    private Member donor;
-    private Member borrower;
+public abstract class DVD extends Item {
+    private String director;
+    private List<String> audioLanguages;
     
-    public DVD (String title, String language, Member donor){
-        this.title = title;
-        this.language = language;
-        this.donor = donor;
-        this.borrower = null;
+    public DVD (String title, String caseLanguage, String director, List<String> audioLanguages, Member donor){
+        super(title, caseLanguage, donor);
+        this.director = director;
     }
     
-    public boolean isOnLoan(){
-        return borrower != null;
+    public String getDirector(){
+        return director;
     }
     
-    public boolean lendTo(Member member){
-        if (isOnLoan()) return false;
-        borrower = member;
-        return true;
+    public void setDirector(String director){
+        this.director = director;
     }
     
-    public void returnItem(){
-        borrower = null;
+    public List<String> getAudioLanguage(){
+        return Collections.unmodifiableList(audioLanguages);
+    }
+    
+    public void addAudioLangauge(String language){
+        audioLanguages.add(language);
+    }
+    
+    public void removeAudioLanguage(String language){
+        audioLanguages.remove(language);
+    }
+    
+    @Override
+    public String toString(){
+        return "DVD: " + getTitle() + " (" + director + ")";
     }
     
 }
