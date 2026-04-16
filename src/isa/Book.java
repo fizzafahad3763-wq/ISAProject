4985ab32c10c4984069f63df5be69b35cf2b3fd7
ac24aci <ac24aci@herts.ac.uk>
2026-04-16 18:55:23 +0100

    Book Class

--- src/isa/Book.java
+++ src/isa/Book.java
@@ -8,6 +8,42 @@ package isa;
  *
  * @author saniafarooq
  */
-public class Book {
-    
+public class Book extends Item {
+
+    private String author;
+    private String isbn;
+
+    public Book(String title, String author, Member donatedBy, String language, String isbn) {
+        super(title, language, donatedBy);
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book(" +
+                "Title:" + getTitle() +
+                "Author:" + author +
+                ",ISBN:" + isbn +
+                ",Language:"+ getLanguage() +
+                ")";
+        
+        
+    }
 }
--- src/isa/Item.java
+++ src/isa/Item.java
@@ -8,6 +8,61 @@ package isa;
  *
  * @author saniafarooq
  */
-public class Item {
+public abstract class Item {
     
+    private String title;
+    private String language;
+    private Member donatedBy;
+    private Member onLoanTo;
+
+    public Item(String title, String language, Member donatedBy) {
+        this.title = title;
+        this.language = language;
+        this.donatedBy = donatedBy;
+        this.onLoanTo = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public Member getDonator() {
+        return donatedBy;
+    }
+    
+    public void clearDonator() {
+        this.donatedBy = null;
+    }
+      
+    public void loanTo(Member borrower) {
+        if (isAvailable()) {
+            this.onLoanTo = borrower;
+        
+        } else{
+                    System.out.println("Item is currenlty being loaned");
+        }
+    }
+    
+    public boolean isAvailable() {
+        return onLoanTo == null;
+    }
+
+    public void returnLoan() {
+        if (onLoanTo != null) {
+            onLoanTo.returnItem(this);
+            this.onLoanTo = null;
+        }
+    }
 }
